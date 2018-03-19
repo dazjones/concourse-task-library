ba4cfbba45359d448b820038af320521b866102f
Darren Jones <darren@darrenjonesdesign.co.uk>
2018-03-19 10:55:38 +0000

    Added update-github-release script

---
+++ update-manifest/update-github-release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RELEASE_FILE=`find gcs-bucket -name '*.tgz'`
+RELEASE_NUMBER=$(echo ${RELEASE_FILE} | sed -e 's:.*-\([0-9]\+\.[0-9]\+\.[0-9]\+\).*:\1:')
+RELEASE_SHA=$(shasum ${RELEASE_FILE} | awk '{ print $1 }')
+RELEASE_URL=https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/download/v${RELEASE_NUMBER}/${RELEASE}-${RELEASE_NUMBER}.tgz
+
+cat <<EOF > git-buildstack-deployment/new-version.yml
+---
+releases:
+- name: ${RELEASE}
+  url: ${RELEASE_URL}
+  version: ${RELEASE_NUMBER}
+  sha1: ${RELEASE_SHA}
+EOF
+
+cat git-buildstack-deployment/new-version.yml
+
+spruce merge git-buildstack-deployment/${YAML_FILE} git-buildstack-deployment/new-version.yml > git-buildstack-deployment-modified/${YAML_FILE}
+
+pushd git-buildstack-deployment-modified
+  cat ${YAML_FILE}
+  git config user.name "${GIT_COMMIT_USERNAME}"
+  git config user.email "${GIT_COMMIT_EMAIL}"
+  if [[ -n $(git status --porcelain) ]]; then
+    echo "Changes"
+    git add .
+    git commit -m "Updated ${RELEASE} release to version ${RELEASE_NUMBER}"
+  else
+    echo "No changes"
+  fi
+popd
